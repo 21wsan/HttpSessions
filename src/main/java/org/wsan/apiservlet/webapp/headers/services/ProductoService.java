9d8b3eb017c10e90ec481c45da21fcd313ee1263
WILLINTONG SANCHEZ <SANCHEZNW@previsora.col>
2024-12-16 16:06:45 -0500

    clase 433 - ver carro compras - funcional

--- src/main/java/org/wsan/apiservlet/webapp/headers/services/ProductoService.java
+++ src/main/java/org/wsan/apiservlet/webapp/headers/services/ProductoService.java
@@ -3,8 +3,11 @@ package org.wsan.apiservlet.webapp.headers.services;
 import org.wsan.apiservlet.webapp.headers.models.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoService {
 
     List<Producto> listar();
+
+    Optional<Producto> porId(Long id);
 }
--- src/main/java/org/wsan/apiservlet/webapp/headers/services/ProductoServiceImpl.java
+++ src/main/java/org/wsan/apiservlet/webapp/headers/services/ProductoServiceImpl.java
@@ -4,6 +4,7 @@ import org.wsan.apiservlet.webapp.headers.models.Producto;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoServiceImpl implements ProductoService{
     @Override
@@ -11,5 +12,10 @@ public class ProductoServiceImpl implements ProductoService{
         return Arrays.asList(new Producto(1L, "notebook", "computacion", 175000),
                              new Producto(2L, "mesa de escritorio", "oficina", 10000),
                              new Producto(3L, "teclado mecanico", "computacion", 40000));
+    }
+
+    @Override
+    public Optional<Producto> porId(Long id) {
+        return listar().stream().filter(p -> p.getId().equals(id)).findAny();
     }
 }
