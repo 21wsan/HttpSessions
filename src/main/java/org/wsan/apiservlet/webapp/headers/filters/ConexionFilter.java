f44a5f988799fcaa57ac21b255a3562461c8e7df
WILLINTONG SANCHEZ <SANCHEZNW@previsora.col>
2024-12-30 09:15:21 -0500

    App Funcionando antes de base de datos
    se deja funcionando la conexión a mysql

--- src/main/java/org/wsan/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ src/main/java/org/wsan/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -1,2 +1,36 @@
-package org.wsan.apiservlet.webapp.headers.filters;public class ConexionFilter {
+package org.wsan.apiservlet.webapp.headers.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.wsan.apiservlet.webapp.headers.util.ConexionBaseDatos;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+//se ejecuta para cualquier ruta de la aplicación con @WebFilter("/*")
+@WebFilter("/*")
+public class ConexionFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try (Connection conn = ConexionBaseDatos.getConnection()){
+
+            if(conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+                request.setAttribute("conn", conn);
+                chain.doFilter(request, response);
+                conn.commit();
+            }catch (SQLException e){
+                conn.rollback();
+                ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
