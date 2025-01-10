c4c32691239dd7b7da5533ac3b269d641eb28a51
WILLINTONG SANCHEZ <SANCHEZNW@previsora.col>
2025-01-10 12:40:46 -0500

    revisando Clase 444
    Se configura guardar, actualizar y eliminar desde la clase ProductoRepositoryJdbcImpl

--- src/main/java/org/wsan/apiservlet/webapp/headers/repositories/ProductoRepositoryJdbcImpl.java
+++ src/main/java/org/wsan/apiservlet/webapp/headers/repositories/ProductoRepositoryJdbcImpl.java
@@ -7,21 +7,21 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductoRepositoryJdbcImpl implements Repository<Producto>{
+public class ProductoRepositoryJdbcImpl implements Repository<Producto> {
 
     private Connection conn;
 
-    public ProductoRepositoryJdbcImpl(Connection conn){
+    public ProductoRepositoryJdbcImpl(Connection conn) {
         this.conn = conn;
     }
 
     @Override
     public List<Producto> listar() throws SQLException {
         List<Producto> productos = new ArrayList<>();
-        try(Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT p.*, c.nombre as categoria FROM productos as p " +
-                    " inner join categorias as c ON (p.categoria_id = c.id) order by p.id ASC")){
-            while(rs.next()){
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT p.*, c.nombre as categoria FROM productos as p " +
+                     " inner join categorias as c ON (p.categoria_id = c.id) order by p.id ASC")) {
+            while (rs.next()) {
                 Producto p = getProducto(rs);
 
                 productos.add(p);
@@ -34,12 +34,12 @@ public class ProductoRepositoryJdbcImpl implements Repository<Producto>{
     @Override
     public Producto porId(Long id) throws SQLException {
         Producto producto = null;
-        try(PreparedStatement stmt = conn.prepareStatement("SELECT p.*, c.nombre as categoria FROM productos as p " +
-                 " inner join categorias  as c ON (p.categoria_id = c.id) WHERE p.id = ?")){
+        try (PreparedStatement stmt = conn.prepareStatement("SELECT p.*, c.nombre as categoria FROM productos as p " +
+                " inner join categorias  as c ON (p.categoria_id = c.id) WHERE p.id = ?")) {
             stmt.setLong(1, id);
 
-            try(ResultSet rs = stmt.executeQuery()){
-                if(rs.next()){
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
                     producto = getProducto(rs);
                 }
             }
@@ -49,12 +49,33 @@ public class ProductoRepositoryJdbcImpl implements Repository<Producto>{
 
     @Override
     public void guardar(Producto producto) throws SQLException {
-
+        String sql;
+        if (producto.getId() != null && producto.getId() > 0) {
+            sql = "update productos ser nombre=?, sku=?, categoria_id=?, where id=?";
+        } else {
+            sql = "insert into productos (nombre, precio, sku, categoria_id, fecha_registro) values (?,?,?,?,?)";
+        }
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, producto.getNombre());
+            stmt.setInt(2, producto.getPrecio());
+            stmt.setString(3, producto.getSku());
+            stmt.setLong(4, producto.getCategoria().getId());
+            if (producto.getId() != null && producto.getId() > 0) {
+                stmt.setLong(5, producto.getId());
+            } else {
+                stmt.setDate(5, Date.valueOf(producto.getFechaRegistro()));
+            }
+            stmt.executeUpdate();
+        }
     }
 
     @Override
     public void eliminar(Long id) throws SQLException {
-
+        String sql = "delete from productos where id=?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+        }
     }
 
     private static Producto getProducto(ResultSet rs) throws SQLException {
