ff25d58e6041e3d6723089db823c183eb88bcbba
WILLINTONG SANCHEZ <SANCHEZNW@previsora.col>
2025-02-17 16:33:19 -0500

    Clase 474 Manejo de transacciones usando interceptores y anotaciones

--- src/main/java/org/wsan/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ src/main/java/org/wsan/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -15,29 +15,30 @@ import java.sql.SQLException;
 @WebFilter("/*")
 public class ConexionFilter implements Filter {
 
-    @Inject
+    /*@Inject
     @MysqlConn
     private Connection conn;
-
+    */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-            try {
+            /*try {
                 Connection connRequest = this.conn;
             if(connRequest.getAutoCommit()){
                 connRequest.setAutoCommit(false);
-            }
+            }*/
             try {
                 //request.setAttribute("conn", connRequest);
                 chain.doFilter(request, response);
-                connRequest.commit();
-            }catch (SQLException | ServiceJdbcException e){
-                connRequest.rollback();
+                //connRequest.commit();
+            }catch (ServiceJdbcException e){
+                //connRequest.rollback();
                 ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
                 e.printStackTrace();
              }
+            /*
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }
+        }*/
     }
 }
